6d20666b31db4d5bab603e149464e5d8998a0d5f
tmtruong1607 <tm.truong1607@gmail.com>
2020-09-24 22:46:28 +0700

    First commit

---
+++ Test Suites/Login.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>db359882-bd1c-4477-9d91-584af3e12bbd</testSuiteGuid>
+   <testCaseLink>
+      <guid>f4978076-e85a-4180-84bb-ea33c27ebdb6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1-Login_wrong_password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>12680c20-f315-474d-a685-66fe9f0d868c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC3-Login_lower_case_id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8d5f90aa-1ba9-4471-8886-e5deccc4f859</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2-Login_success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0453d896-86de-4af2-aa5e-ead5ff671a06</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC4-Login_upper_case_id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e7c97f4c-809f-4b3c-847c-592dbb80f05d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC5-Login_add_a_space_to_password</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
